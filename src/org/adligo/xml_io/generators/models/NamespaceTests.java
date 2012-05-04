33e637bfb5608942f5177cb005245ea066d0efcf
scott <scott@adligo.com>
2012-05-04 05:45:17 +0000

    added tests, attributes are going to xml yea

---
+++ src/org/adligo/xml_io/generators/models/NamespaceTests.java
@@ -0,0 +1,15 @@
+package org.adligo.xml_io.generators.models;
+
+import org.adligo.tests.ATest;
+import org.adligo.xml_io.generator.models.Namespace;
+
+public class NamespaceTests extends ATest {
+
+	public void testNamespace() {
+		String result = Namespace.toNamespace("org.adligo.foo");
+		assertEquals("http://www.adligo.org/foo", result);
+		
+		result = Namespace.toNamespace("org.adligo.foo.client");
+		assertEquals("http://www.adligo.org/foo/client", result);
+	}
+}
